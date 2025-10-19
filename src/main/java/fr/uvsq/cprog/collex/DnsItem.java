1fbf2d73fc967673e9782c5ad09277db7c96d56b
uvsq22502914 <anais.djadel06@gmail.com>
2025-10-19 19:26:10 +0200

    Ajout de la classe DnsItem avec equals, hashCode et toString

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -1,24 +1,39 @@
 package fr.uvsq.cprog.collex;
 
-public class DnsItem {
-    private String nom;
-    private String adresseIP;
+import java.util.Objects;
 
-    public DnsItem(String nom, String adresseIP) {
-        this.nom = nom;
+public class DnsItem {
+    private final NomMachine nomMachine;
+    private final AdresseIP adresseIP;
+
+    public DnsItem(NomMachine nomMachine, AdresseIP adresseIP) {
+        this.nomMachine = nomMachine;
         this.adresseIP = adresseIP;
     }
 
-    public String getNom() {
-        return nom;
+    public NomMachine getNomMachine() {
+        return nomMachine;
     }
 
-    public String getAdresseIP() {
+    public AdresseIP getAdresseIP() {
         return adresseIP;
     }
 
     @Override
     public String toString() {
-        return nom + " -> " + adresseIP;
+        return nomMachine.toString() + " -> " + adresseIP.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DnsItem)) return false;
+        DnsItem dnsItem = (DnsItem) o;
+        return nomMachine.equals(dnsItem.nomMachine) && adresseIP.equals(dnsItem.adresseIP);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomMachine, adresseIP);
     }
 }
