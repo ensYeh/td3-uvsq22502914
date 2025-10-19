c0f2a385a5d15b21e7e4837f68a034a3d66cc25f
uvsq22502914 <anais.djadel06@gmail.com>
2025-10-19 17:28:16 +0200

    Implémentation de la gestion des commandes dans DnsTUI avec nextCommande

--- src/main/java/fr/uvsq/cprog/DnsTUI.java
+++ src/main/java/fr/uvsq/cprog/DnsTUI.java
@@ -1,6 +1,9 @@
 package fr.uvsq.cprog;
 
 import java.util.Scanner;
+import fr.uvsq.cprog.collex.Dns;
+import fr.uvsq.cprog.Commande;
+import fr.uvsq.cprog.RechercheIpCommande;
 
 /**
  * Classe pour interagir avec l'utilisateur (TUI = Text User Interface).
@@ -27,5 +30,42 @@ public class DnsTUI {
      */
     public void affiche(Object o) {
         System.out.println(o);
+    }
+
+    /**
+     * Analyse la ligne de commande et renvoie la commande correspondante.
+     * @param dns l'objet Dns pour manipuler les entrées DNS
+     * @return une instance de Commande ou null si commande invalide
+     */
+    public Commande nextCommande(Dns dns) {
+        String ligne = nextInput();
+        String[] parties = ligne.split("\\s+");
+
+        if (parties.length == 0 || parties[0].isEmpty()) {
+            return null;
+        }
+
+        String cmd = parties[0];
+        switch (cmd) {
+            case "rechercheip":
+                if (parties.length < 2) {
+                    affiche("ERREUR : il manque le nom de machine");
+                    return null;
+                }
+                return new RechercheIpCommande(dns, parties[1]);
+
+            case "quitter":
+                return new Commande() {
+                    @Override
+                    public void execute() {
+                        System.out.println("Au revoir !");
+                        System.exit(0);
+                    }
+                };
+
+            default:
+                affiche("Commande inconnue : " + cmd);
+                return null;
+        }
     }
 }
